cf65beefd5cf24902283b389d9341ddc86d357ca
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2023-01-07 12:51:44 +0100

    created the multi-filter form component

---
+++ src/components/forms/filterUsers/index.tsx
@@ -0,0 +1,41 @@
+import { FormEventHandler, ReactElement } from 'react';
+import './style.scss';
+
+export default (): ReactElement<HTMLFormElement> => {
+    // states
+    // methods
+    const onSubmit: FormEventHandler<HTMLFormElement> = (e): void => {
+        e.preventDefault();
+    };
+
+    return <form action="#" className="filter-users" onSubmit={onsubmit}>
+        <div className="form-group">
+            <div className="input-group"><label htmlFor="organisation">organisation</label>
+                <select name="organisation">
+                    <option value="" hidden>select</option>
+                </select>
+            </div>
+            <div className="input-group"><label htmlFor="username">username</label>
+                <input type="text" name='username' placeholder='user' />
+            </div>
+            <div className="input-group"><label htmlFor="email">email</label>
+                <input type="email" name='email' placeholder='email' />
+            </div>
+            <div className="input-group"><label htmlFor="date">date</label>
+                <input type="date" name='date' placeholder='date' />
+            </div>
+            <div className="input-group"><label htmlFor="phone_number">phone number</label>
+                <input type="tel" name='phone_number' placeholder='phone number' />
+            </div>
+            <div className="input-group"><label htmlFor="date">status</label>
+                <select name="status">
+                    <option value="" hidden>select</option>
+                </select>
+            </div>
+        </div>
+        <div className="btn">
+            <button type='reset'>reset</button>
+            <button type='submit'>filter</button>
+        </div>
+    </form>;
+};
--- src/components/tables/users/index.tsx
+++ src/components/tables/users/index.tsx
@@ -1,11 +1,12 @@
 import './style.scss';
 import * as cards from '../../cards';
+import * as forms from '../../forms';
 import { tablesType } from '../../../vite-env';
 import { usersDataTable } from '../../../data';
 import { UserContext } from '../../../contexts';
 import sort from '../../../assets/icons/sort.svg';
-import { ReactElement, useMemo, useContext, ReactEventHandler, ChangeEventHandler } from 'react';
-import { useTable, useSortBy, HeaderGroup, usePagination } from 'react-table';
+import { ReactElement, useMemo, useContext, ChangeEventHandler, Fragment } from 'react';
+import { useTable, useSortBy, HeaderGroup, usePagination, UsePaginationOptions, TableOptions, UseTableHeaderGroupProps } from 'react-table';
 
 export default (): ReactElement<HTMLTableElement> => {
     // context
@@ -21,47 +22,50 @@ export default (): ReactElement<HTMLTableElement> => {
         getTableProps, getTableBodyProps, prepareRow, pageOptions,
         page, nextPage, previousPage, canNextPage, canPreviousPage,
         gotoPage, pageCount, state: { pageIndex },
-    } = useTable({ columns, data }, useSortBy, usePagination);
+    }: TableOptions<any> & UsePaginationOptions = useTable({ columns, data }, useSortBy, usePagination);
 
     // methods
     const gotoSelectedPage: ChangeEventHandler<HTMLSelectElement> = ({ currentTarget: t }) => gotoPage(t?.value || 1);
 
-    return <table {...getTableProps()} className="users">
-        <thead>
-            {headerGroups.map(headerGroup => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                    {
-                        headerGroup.headers.map((column: HeaderGroup, index) => (
-                            <th {...column.getHeaderProps(column.getSortByToggleProps())} className={`${column?.isSorted ? 'sort' : ''}`}>
-                                {column.render('Header')} {index < headerGroup.headers.length - 1 ? <img src={sort} alt="" className="sort" style={{ transform: `rotate(${column.isSortedDesc ? 180 : 0}deg)` }} /> : null}
-                            </th>
-                        ))
-                    }
-                </tr>))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-            {page.map(row => {
-                prepareRow(row);
-                return <tr {...row.getRowProps()}>{row.cells.map((cell, index) => (
-                    <td {...cell.getCellProps()}>{index < row.cells.length - 1 ? cell.render('Cell') : <cards.menu id={row.values.id} />}</td>))}
-                </tr>;
-            })}
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colSpan={5}>
-                    <span>showing
-                        <select name="page" onSelect={gotoSelectedPage} onChange={gotoSelectedPage}>
-                            {Array(pageOptions.length).fill(0).map((_, index) => <option key={index} value={index + 1} selected={index == pageIndex}>{index + 1}</option>)}
-                            <option value=""></option>
-                        </select>
-                        out of {pageOptions.length}</span>
-                </td>
-                <td colSpan={2}>
-                    <button disabled={!canPreviousPage} onClick={previousPage}>previous</button>
-                    <button disabled={!canNextPage} onClick={nextPage}>next</button>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+    return <Fragment>
+        <forms.filter />
+        <table {...getTableProps()} className="users">
+            <thead>
+                {headerGroups.map((headerGroup: UseTableHeaderGroupProps<any>) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {
+                            headerGroup.headers.map((column: HeaderGroup, index) => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())} className={`${column?.isSorted ? 'sort' : ''}`}>
+                                    {column.render('Header')} {index < headerGroup.headers.length - 1 ? <img src={sort} alt="" className="sort" style={{ transform: `rotate(${column.isSortedDesc ? 180 : 0}deg)` }} /> : null}
+                                </th>
+                            ))
+                        }
+                    </tr>))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {page.map(row => {
+                    prepareRow(row);
+                    return <tr {...row.getRowProps()}>{row.cells.map((cell, index) => (
+                        <td {...cell.getCellProps()}>{index < row.cells.length - 1 ? cell.render('Cell') : <cards.menu id={row.values.id} />}</td>))}
+                    </tr>;
+                })}
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colSpan={5}>
+                        <span>showing
+                            <select name="page" onSelect={gotoSelectedPage} onChange={gotoSelectedPage} defaultValue={pageIndex + 1}>
+                                {Array(pageOptions.length).fill(0).map((_, index) => <option key={index} value={index + 1} >{index + 1}</option>)}
+                                <option value=""></option>
+                            </select>
+                            out of {pageOptions.length}</span>
+                    </td>
+                    <td colSpan={2}>
+                        <button disabled={!canPreviousPage} onClick={previousPage}>&lt;</button>
+                        <button disabled={!canNextPage} onClick={nextPage}>&gt;</button>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+    </Fragment>;
 };
